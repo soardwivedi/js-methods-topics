86e94df833f4b138727b76eada2df51c5080ed96
Vivek <soarvivekdwivedi@gmail.com>
2024-09-21 17:31:57 +0530

    http server added

--- Node/httpServer/simpleHTTPServer1.js
+++ Node/httpServer/simpleHTTPServer1.js
@@ -1,1 +1,11 @@
 const http = require('http');
+const PORT = 3003;
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('some content');
+  res.end(JSON.stringify({ message: 'Hello, World!' }));
+});
+
+server.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
