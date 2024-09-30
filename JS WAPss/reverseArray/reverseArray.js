bd735dd5f96db14a630a367f1dd9c4b062c553f8
Vivek <soarvivekdwivedi@gmail.com>
2024-09-30 12:00:48 +0530

    reverse array

---
+++ JS WAPss/reverseArray/reverseArray.js
@@ -0,0 +1,23 @@
+function reverseArray(arr) {
+  let left = 0;
+  let right = arr.length - 1;
+
+  while (left < right) {
+    // Swap elements
+    let temp = arr[left];
+    // console.log(temp, arr[left], arr[right]);
+    arr[left] = arr[right];
+    arr[right] = temp;
+
+    // Move towards the center
+    left++;
+    console.log(left);
+    right--;
+  }
+
+  return arr;
+}
+
+// Example usage:
+let array = [1, 2, 3, 4, 5];
+console.log(reverseArray(array)); // Output: [5, 4, 3, 2, 1]
