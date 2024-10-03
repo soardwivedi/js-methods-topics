42bd5e2a7c888640416fac07c8f45f9ecd7b9040
Vivek <soarvivekdwivedi@gmail.com>
2024-10-04 00:00:57 +0530

    2nd largest by loop

--- JS_topics_Methods/callApplyBind/callApplyBind.js
+++ JS_topics_Methods/callApplyBind/callApplyBind.js
@@ -1,28 +1,30 @@
 let name = {
-    firstName: "Akshay",
-    lastName: "Saini",  
-}
+  firstName: 'Akshay',
+  lastName: 'Saini'
+};
 
-let printFullName = function(hometown, state){
-    console.log(this.firstName+" "+ this.lastName+ " from "+ hometown+ ", "+ state);
-}
+let printFullName = function (hometown, state) {
+  console.log(
+    this.firstName + ' ' + this.lastName + ' from ' + hometown + ', ' + state
+  );
+};
 // Call Method
 
-printFullName.call(name, "Deharadun", "Uttarakhand");
+printFullName.call(name, 'Deharadun', 'Uttarakhand');
 
 let name2 = {
-    firstName: "Sachin",
-    lastName: "Tendulkar"
-}
+  firstName: 'Sachin',
+  lastName: 'Tendulkar'
+};
 
-// function borrowing.=> we can borrow functions from 
+// function borrowing.=> we can borrow functions from
 // other object and use it with the data of other object.
-printFullName.call(name2, "Mumbai", "Maharastra")
+printFullName.call(name2, 'Mumbai', 'Maharastra');
 
 // Apply Method
-printFullName.apply(name2, ["MumbaiMumbai", "Maharastra"]);
+printFullName.apply(name2, ['MumbaiMumbai', 'Maharastra']);
 
 // Bind Method
-let printMyName = printFullName.bind(name2, "Mumbai", "Maharastra");
+let printMyName = printFullName.bind(name2, 'Mumbai', 'Maharastra');
 console.log(printMyName);
 printMyName();
