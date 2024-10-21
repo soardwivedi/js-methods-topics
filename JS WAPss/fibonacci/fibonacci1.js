bca20541436e53457d6a2d73f846ab7d69d1b12d
Vivek <soarvivekdwivedi@gmail.com>
2024-10-21 23:58:00 +0530

    fibonacci

---
+++ JS WAPss/fibonacci/fibonacci1.js
@@ -0,0 +1,17 @@
+const { text } = require('express');
+
+const num = 5;
+
+function fibonacci(num) {
+  let num1 = 0;
+  let num2 = 1;
+  let temp;
+  console.log(num1, num2);
+  for (let i = 0; i < num; i++) {
+    temp = num1 + num2;
+    console.log(temp);
+    num1 = num2;
+    num2 = temp;
+  }
+  return;
+}
