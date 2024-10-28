83d9fe400d635b76ac258530b04fa44acdbee17a
Vivek <soarvivekdwivedi@gmail.com>
2024-10-28 23:11:26 +0530

    some commit

--- JS WAPss/string elements in array/stringElementInArray.js
+++ JS WAPss/string elements in array/stringElementInArray.js
@@ -2,7 +2,7 @@ let str = 'Hello World';
 let arr = [];
 
 for (let i = 0; i < str.length; i++) {
-  if (str[i] !== ' ') arr[i] = str[i];
+  if (str[i] !== ' ' || str[i] != undefined) arr[i] = str[i];
   console.log(arr[i]);
   console.log(str[i]);
 }
