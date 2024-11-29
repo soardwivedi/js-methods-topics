06a34252c2bd3c6552805ffd1c50e2d33db49fb6
Vivek <soarvivekdwivedi@gmail.com>
2024-11-29 14:32:18 +0530

    promises

--- JS_topics_Methods/Promises/promise.js
+++ JS_topics_Methods/Promises/promise.js
@@ -1,7 +1,8 @@
-const myPromise = new Promise((resolve, reject)=>{
-const success = true;
-})
-
-const myPromise1 = new Promise((resolve, reject)=>{
-
+const myPromise = new Promise((resolve, reject) => {
+  if (reject) return 'false';
+  else {
+    const success = console.log(true);
+  }
 });
+
+const myPromise1 = new Promise((resolve, reject) => {});
