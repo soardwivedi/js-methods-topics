b25e9f9f6be3fcc951d8b99cd6a1937dee8da84d
Vivek <soarvivekdwivedi@gmail.com>
2024-12-09 23:05:38 +0530

    every

--- JS_topics_Methods/JS Array Methods/every/every.js
+++ JS_topics_Methods/JS Array Methods/every/every.js
@@ -1,7 +1,5 @@
 const ages = [32, 33, 16, 40];
-
-console.log(ages.every(checkAge));
-
 function checkAge(age) {
   return age > 18;
 }
+console.log(ages.every(checkAge));
--- JS_topics_Methods/JS Array Methods/splice/splice.js
+++ JS_topics_Methods/JS Array Methods/splice/splice.js
@@ -1,7 +1,7 @@
-let arr = ["a", "b", "d", "e", "f", "g"];
+let arr = ['a', 'b', 'd', 'e', 'f', 'g'];
 console.log(arr);
 
-const newArr = arr.splice(0, 3, "1", "2", 4);
+const newArr = arr.splice(0, 3, '1', '2', 4, []);
 
 console.log(newArr);
 
