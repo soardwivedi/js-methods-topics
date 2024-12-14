d3e2a724fb74387aa0747b0431ea3f40a0e3f61c
Vivek <soarvivekdwivedi@gmail.com>
2024-12-14 22:00:22 +0530

    add string without using inbuilt methods

---
+++ JS_topics_Methods/addStringsWithoutInbuiltMethod/addString.js
@@ -0,0 +1,9 @@
+let str = 'Hello'; // Initial string
+let newValue = ' World'; // Value to be added
+
+// Adding the new value manually
+for (let i = 0; i < newValue.length; i++) {
+  str += newValue[i];
+}
+
+console.log(str); // Output: "Hello World"
