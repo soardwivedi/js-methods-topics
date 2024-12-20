9eeb10c61927153038f9df9b03a0bafd08315f7a
Vivek <soarvivekdwivedi@gmail.com>
2024-12-20 20:03:58 +0530

    duplicate removed

--- JS WAPss/removeDuplicate/removeDuplicate1.js
+++ JS WAPss/removeDuplicate/removeDuplicate1.js
@@ -2,10 +2,12 @@ const arr = [1, 3, 2, 2, 4, 5, 4, 6];
 const removeDuplicates = (arr) => {
   let newArray = [];
   let uniqueElements = {};
+  let index = 0;
   for (let i = 0; i < arr.length; i++) {
     if (!uniqueElements[arr[i]]) {
       uniqueElements[arr[i]] = true;
-      newArray[arr[i]] = arr[i];
+      newArray[index] = arr[i];
+      index++;
     }
   }
   return newArray;
