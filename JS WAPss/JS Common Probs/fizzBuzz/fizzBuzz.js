24e9426f97dd74af32d68e1e76b41835650af4b2
vivek_dwivedi <vivek.dwivedi@mounttalent.com>
2025-01-30 17:37:11 +0530

    json added

--- JS WAPss/JS Common Probs/fizzBuzz/fizzBuzz.js
+++ JS WAPss/JS Common Probs/fizzBuzz/fizzBuzz.js
@@ -1,32 +1,22 @@
-// function fizzBuzz(x, y){
-//    for(let i = x; i <= y; i++)
-//     {
-//     if(i % 2 == 0){
-//         console.log("fizz");
-//     }
-//    else if(i % 3 == 0){
-//         console.log("buzz");
-//     }
-//    else if(i % 2 && i % 3 == 0){
-//         console.log("fizzBuzz");
-//     }
-//     else console.log(i);
-// }
-// }
+function fizzBuzz(y) {
+  for (let i = 0; i <= y; i++) {
+    if (i % 2 == 0 && i % 3 == 0) {
+      console.log('fizzBuzz');
+    } else if (i % 3 == 0) {
+      console.log('buzz');
+    } else if (i % 2 == 0) {
+      console.log('fizz');
+    }
+    // else console.log(i);
+  }
+}
 
-// console.log(fizzBuzz(4, 13));
+console.log(fizzBuzz(16));
 
+// for (let i = 1; i < 15; )
+//   console.log((++i % 3 ? '' : 'Fizz') + (i % 5 ? '' : 'Buzz') || i);
 
+// console.log('Ram');
 
-
-
-
-
-
-for (let i = 1; i < 15;) console.log((++i % 3 ? '' : 'Fizz') + (i % 5 ? '' : 'Buzz') || i)
-
-
-
-console.log("Ram");
-
-for (let i = 1; i < 15;) console.log((++i % 3 ? "" : "Fizz") + (i % 5 ? "" : "Buzz") || i);
+// for (let i = 1; i < 15; )
+//   console.log((++i % 3 ? '' : 'Fizz') + (i % 5 ? '' : 'Buzz') || i);
--- JS WAPss/removeDuplicate/removeDuplicate2.js
+++ JS WAPss/removeDuplicate/removeDuplicate2.js
@@ -1,4 +1,10 @@
 const removeDuplicates = (arr) => {
   let newArray = [];
   let index = 0;
+  for(let i = 0; i< arr.length; i++){
+if(){
+
+}
+  }
+  
 };
