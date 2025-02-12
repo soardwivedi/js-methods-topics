61b1952dfd2a511df770e5619fd5cb46ef46cea4
vivek_dwivedi <vivek.dwivedi@mounttalent.com>
2025-02-12 22:58:37 +0530

    import export

---
+++ Node/Import Export/app.js
@@ -0,0 +1,7 @@
+import sumFun from './sum';
+
+let a = 6;
+let b = 5;
+
+const sum = sumFun(a, b);
+console.log('sum of nums = ', sum);
---
+++ Node/Import Export/sum.js
@@ -0,0 +1,6 @@
+const sumFun = function (a, b) {
+  let sum = a + b;
+  console.log(sum);
+  return sum;
+};
+module.exports = sumFun;
